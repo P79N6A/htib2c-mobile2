2459a7699a5497fb39ea13e51dfcc9bfd084537d
liuning <18505470185@163.com>
2017-10-11 17:34:18 +0800

    删除

--- src/main/java/com/htichina/web/message/LocationEventRep.java
+++
@@ -1,7 +0,0 @@
-package com.htichina.web.message;
-
-/**
- * Created by yaliu on 2017/5/2.
- */
-public class LocationEventRep {
-}
--- src/main/java/com/htichina/wsclient/payment/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://payment.ws.htichina.com/")
-package com.htichina.wsclient.payment;
