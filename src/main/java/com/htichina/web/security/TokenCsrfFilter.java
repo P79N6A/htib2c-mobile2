b39018f1855cc5b8877977f42d9fd9fa3168861e
liuning <18505470185@163.com>
2018-01-26 17:30:48 +0800

    CR389

--- src/main/java/com/htichina/web/security/TokenCsrfFilter.java
+++ src/main/java/com/htichina/web/security/TokenCsrfFilter.java
@@ -72,9 +72,9 @@ public class TokenCsrfFilter implements Filter {
 				   logger.info("-------------------------------------uri2:"+uri);
 			       chain.doFilter(request, response); 
 			   } else{ 
-				   //logger.info("-------------------------------------uri3:"+uri);
-				   //logger.info("-------------------------------------session token:"+sToken);
-				   //logger.info("-------------------------------------parameter token:"+pToken);
+				   logger.info("-------------------------------------uri3:"+uri);
+				   logger.info("-------------------------------------session token:"+sToken);
+				   logger.info("-------------------------------------parameter token:"+pToken);
 				  
 				   request.getRequestDispatcher("/views/common/error.xhtml").forward(request,response); 
 			   } 
@@ -136,7 +136,8 @@ public class TokenCsrfFilter implements Filter {
 		   uri.contains(".png")||
 		   uri.contains(".ico")||
 		   uri.contains(".jpg")||
-		   uri.contains(".gif")){
+		   uri.contains(".gif")||
+			uri.contains(".ttf")){
 			return true;
 		}  
 		return false;
