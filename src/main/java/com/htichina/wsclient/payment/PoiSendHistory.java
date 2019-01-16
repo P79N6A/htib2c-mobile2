f4c3be6c12323efd2719c3ac0312b53a9e1e98a0
liuning <18505470185@163.com>
2019-01-16 15:33:37 +0800

    poi发送

---
+++ src/main/java/com/htichina/wsclient/payment/PoiSendHistory.java
@@ -0,0 +1,90 @@
+
+package com.htichina.wsclient.payment;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>poiSendHistory complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="poiSendHistory">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="createTime" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/>
+ *         &lt;element name="poiId" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "poiSendHistory", propOrder = {
+    "createTime",
+    "poiId"
+})
+public class PoiSendHistory {
+
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar createTime;
+    protected String poiId;
+
+    /**
+     * 获取createTime属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * 设置createTime属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setCreateTime(XMLGregorianCalendar value) {
+        this.createTime = value;
+    }
+
+    /**
+     * 获取poiId属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPoiId() {
+        return poiId;
+    }
+
+    /**
+     * 设置poiId属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPoiId(String value) {
+        this.poiId = value;
+    }
+
+}
---
+++ src/main/java/com/htichina/wsclient/payment/SelectPoiSendHistory.java
@@ -0,0 +1,87 @@
+
+package com.htichina.wsclient.payment;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>selectPoiSendHistory complex type的 Java 类。
+ * 
+ * <p>以下模式片段指定包含在此类中的预期内容。
+ * 
+ * <pre>
+ * &lt;complexType name="selectPoiSendHistory">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="poiId" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="createTime" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "selectPoiSendHistory", propOrder = {
+    "poiId",
+    "createTime"
+})
+public class SelectPoiSendHistory {
+
+    protected String poiId;
+    protected String createTime;
+
+    /**
+     * 获取poiId属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPoiId() {
+        return poiId;
+    }
+
+    /**
+     * 设置poiId属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPoiId(String value) {
+        this.poiId = value;
+    }
+
+    /**
+     * 获取createTime属性的值。
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * 设置createTime属性的值。
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCreateTime(String value) {
+        this.createTime = value;
+    }
+
+}
